dff082ad75e12afd62a7c6390a987df70126c9b6
Laysonmx <imsecondaryl@gmail.com>
2025-03-18 14:38:27 -0300

    Add files via upload
    espero ter entendido, espero que esteja correto.

---
+++ exercicio.java
@@ -0,0 +1,23 @@
+public class exercicio {
+
+}
+public void main() {
+
+}
+
+public static void main(String[] args) {
+    int nota1 = 40;
+    int nota2 = 10;
+    int nota3 = 20;
+    int nota4 = 30;
+
+
+    int num1 = nota1 + nota2 + nota3 + nota4;
+    int num2 = (num1 / 4);
+
+    System.out.println("Soma:" + num1);
+    System.out.println("Divisão:" + num2);
+}
+
+
+
