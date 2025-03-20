cac12d4cec4bfd4ad5e79255412eb7d1bd1ac634
Laysonmx <imsecondaryl@gmail.com>
2025-03-20 15:58:27 -0300

    Add files via upload

---
+++ casting.java
@@ -0,0 +1,13 @@
+package mod9;
+
+public class casting {
+
+    public static void main(String[] args) {
+
+        int num = 10; // variável primitiva
+        Integer wrappedNum = num; // Autoboxing automático para Integer
+
+
+        System.out.println("Integer: " + wrappedNum);
+    }
+}
