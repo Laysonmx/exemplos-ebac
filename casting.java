fe04017b281c13478edb7cc8647a34fb812320b3
Laysonmx <imsecondaryl@gmail.com>
2025-03-20 14:31:49 -0300

    Add files via upload

---
+++ casting.java
@@ -0,0 +1,18 @@
+package mod9;
+
+public class casting {
+
+    public static void main(String[] args) {
+
+        /* primitiva */
+
+        long cpf = 12345678L;
+
+        /* Wrapped */
+
+        Long l = Long.valueOf(12345678);
+
+        System.out.println(cpf);
+        System.out.println(l);
+    }
+}
